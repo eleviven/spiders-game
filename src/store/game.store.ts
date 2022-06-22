3ae67f2ac8a551fdd96d3134b186e6c86ee01e59
Firuz Hacıyev <firuz.haciyeff@gmail.com>
2022-06-23 03:05:08 +0400

    Added actions, getters & types to game store

--- src/store/game.store.ts
+++ src/store/game.store.ts
@@ -1,11 +1,34 @@
-import { action, makeObservable, observable } from "mobx";
+import { action, computed, makeObservable, observable } from "mobx";
+import { ILevel, ILine, ISpider } from "../types";
 
 export default class GameStore {
+  // State
   @observable level = 1;
-  @observable spiders = {};
-  @observable lines = {};
+  @observable spiders: Record<number, ISpider> = {};
+  @observable lines: Record<number, ILine> = {};
+  @observable timeRecord: number | null = null;
 
-  @action getLevel(id: number) {}
+  // Actions
+  @action setGame(payload: ILevel) {
+    this.level = payload.id;
+    this.spiders = payload.spiders;
+    this.lines = payload.lines;
+  }
+  @action fetchLevel(id: number) {
+    const data = require(`../levels/level-${id}.json`);
+    this.setGame(data);
+  }
+  @action setTimeRecord(value: number) {
+    this.timeRecord = value;
+  }
+
+  //Getters
+  @computed get getSpiders() {
+    return Object.values(this.spiders);
+  }
+  @computed get getLines() {
+    return Object.values(this.lines);
+  }
 
   constructor() {
     makeObservable(this);
